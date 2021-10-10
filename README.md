44b753758230d3988a6e67b44c3e2034e36c59c5
Raihan Munim <raihan.srizon@gmail.com>
2021-10-11 01:19:23 +0600

    Final commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Learning Web
 ### Learning web while implementing html, css and js.
 
-Time Spent: 7 hours
+_Time Spent: 7 hours_
 
 
--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const highlightMenu = () => {
   const aboutMenu = document.querySelector('#about-page');
   const servicesMenu = document.querySelector('#services-page');
   let scrollPos = window.scrollY;
-  // console.log(scrollPos);
 
   // adds 'highlight' class to my menu items
   if (window.innerWidth > 960 && scrollPos < 600) {
